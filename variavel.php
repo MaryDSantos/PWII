5e096ea796986b813b4556bdb299fccd1baf6ce8
marianaluan <mariana.dos.santoss@outlook.com>
2024-08-15 22:30:37 -0300

    aula de variaveis

--- variavel.php
+++ variavel.php
@@ -45,7 +45,27 @@
 </nav>
 
     <div class="container">
-      <h1>Página inicial do site</h1>
+        <h1>Variaveis em php</h1>
+        <p> $numero = 10; //AQUI É UMA VARIAVEL INTEIRA </p> 
+        <p> $string = "Mariana camisa 10"; </p>
+        <p> $string1 = "Mariana camisa ".$numero; </p>
+        <p> $string2 = "Mariana camisa $numero"; </p>
+        <p> $valordecimal = 10.50; </p>
+        <p> $ativo = true; </p>
+        <?php // Tudo que esta entre as tags do php nao aparece na tela ?>
+
+        <?php // a nao ser que usemos a funçao echo com string?>
+
+        <?php
+        echo "hello world";
+        echo "<br>";
+        $numero = 10;
+        $nome = "Mariana";
+        $frase1 = "<p> $nome camisa $numero frase com paragrafo </p>";
+        $frase2 = $nome. " camisa " .$numero;//cocatenação das antigas
+        echo $frase1;
+        echo "$frase2 é o que tem dentro da variavel";
+        ?>
     </div>
 <script src="bootstrap.bundle.min.js"></script>
 
