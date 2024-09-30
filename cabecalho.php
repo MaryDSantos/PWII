4eda71d66960a1585cbbda3c5007596f08c68134
marianaluan <mariana.dos.santoss@outlook.com>
2024-09-30 07:38:58 -0300

    30/09

--- cabecalho.php
+++ cabecalho.php
@@ -59,6 +59,9 @@
         <li class="nav-item">
           <a class="nav-link" href="novo_produto.php">Novo produto</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="produtos.php">Produto</a>
+        </li>
       </ul>
       <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
