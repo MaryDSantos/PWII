a89ede6de8a02b98375b9ab1af7c2cfeb7480546
marianaluan <mariana.dos.santoss@outlook.com>
2024-10-10 22:19:57 -0300

    10-10

--- conexao.php
+++ conexao.php
@@ -1,25 +1,28 @@
-<?php
-$servidor = "localhost";
-$usuario = "root";
-$senha = "";
-$banco = "pw_bd";
+<?php 
+$servidor = "localhost"; 
+$usuario = "root"; 
+$senha = ""; 
+$banco = "PW_BD"; 
 
 $script = "
     CREATE DATABASE PW_BD;
     USE PW_BD;
-    CREATE TABLE PRODUTOS(
+    CREATE TABLE PRODUTOS (
         ID INT PRIMARY KEY AUTO_INCREMENT,
         DESCRICAO VARCHAR(150) NOT NULL,
         CODIGO_BARRAS VARCHAR(25) NOT NULL,
-        VALOR DECIMAL (10,2) NOT NULL,
-        IMAGEM VARCHAR (50),
-        ATIVO BIT NOT NULL 
+        VALOR DECIMAL(10,2) NOT NULL,
+        IMAGEM VARCHAR(50),
+        ATIVO BIT NOT NULL
     );
+    insert into produtos (descricao, codigo_barra, valor, ativo) values('amendoin','1848654986989495', 5.50, 1);
+    insert into produtos (descricao, codigo_barra, valor, ativo) values('goiabinha','264921581578754', 5.50, 1);
+
 ";
 
-$conexao = new myqli($servidor, $usuario, $senha, $banco);
+$conexao = new mysqli($servidor, $usuario, $senha, $banco);
 
-if($conexao->connect_error){
-    die("falha na conexão" . $conexao->connect_error);
+if ($conexao->connect_error) {
+    die("Falha na conexão: " . $conexao->connect_error);
 }
 ?>
--- produto.php
+++ produto.php
@@ -1,96 +1,119 @@
 <?php include "cabecalho.php"; ?>
-<?php
-    if( isset($_GET["pesquisa"]))
+
+<?php 
+if( isset($_GET["pesquisa"]) )
+{
+    $pesquisa = $_GET["pesquisa"];
+    if( empty($pesquisa) )
     {
-        $pesquisa = $_GET["pesquisa"];
-        if( empty($pesquisa))
-        {
-            //se a variavel estiver vazia executa aqui
-        }else
-        {
-            //aqui vai a lógica da pesquisa
-        }
-    }else
-    {
-        $pesquisa = "";
+       //Se a variavel estiver vazia executa aqui 
     }
+    else
+    {
+        //Aqui vai a lógica da pesquisa
+    }
+}
+else
+{
+    $pesquisa = "";
+    include "conexao.php";
+    $sql = "select id, descricao, valor, codigo_barras from produto order by id desc";
+    $resultado = $conexao->query($sql);
+    if($resulado->num_rows>0){
+        while($row = $resultado->fetch_assoc()){
+            echo "<tr>";
+            echo "<td>" . $row["id"] . "</td>";
+            echo "<td>" . $row["descricao"] . "</td>";
+            echo "<td>" . $row["valor"] . "</td>";
+            echo "<td><a href='editar_produto.php?id=$row[id]' class='btn btn-warning'>editar</a></td>";
+            echo "<a class='btn btn-danger'>excluir</a></td>";    
+            echo "</tr>"
+        }
+        else{
+            echo"<tr><td colspan='3'>nenhum resgistro encontrado</td></tr>";
+        }
+        $conexao->close();
+    }
+}
+
+
 ?>
 <br>
 <div class="row">
     <div class="col-12">
         <div class="card">
             <div class="card-header">
-                Lista de produtos
+                Lista de Produtos
             </div>
             <div class="card-body">
                 <div class="row">
-                    <div class="col-2">                        
-                        <a href="novo_produto.php" class="btn btn-success">
+                    <div class="col-2">
+                        <a href="novo_produto.php" class="btn btn-success" >
                             Novo Produto
-                        </a> 
+                        </a>
                     </div>
-
                     <div class="col-8">
                         <form action="produtos.php" method="get">
-                        <div class="input-group mb-3">
-                            <input 
-                                type="text" 
-                                name="pesquisa" 
-                                class="form-control" 
-                                placeholder="Digite sua pesquisa aqui"
-                                value="<?php echo $pesquisa ?>"/>
-                            <button type="submit" class="btn btn-primary">
-                                Pesquisar
-                            </button>
-                            </div>
+                            <div class="input-group mb-3">
+                                <input type="text" 
+                                        name="pesquisa" 
+                                        value="<?php echo $pesquisa; ?>"  
+                                        class="form-control" 
+                                        placeholder="Digite sua pesquisa aqui..." />
 
+
+                                <button class="btn btn-primary" type="submit">
+                                    Pesquisar
+                                </button>
+                            </div>
                         </form>
                     </div>
-                    <div class="col-2">
-
-                    </div>
-
+                    <div class="col-2"></div>
                 </div>
+
                 <div class="row">
                     <div class="col-12">
+                        
                         <table class="table table-hover table-striped">
                         <thead>
                             <tr>
-                            <th scope="col">Id</th>
-                            <th scope="col">Descrição</th>
-                            <th scope="col">Valor</th>
-                            <th scope="col">Código de barras</th>
-                            <th scope="col">Imagem</th>
-                            <th scope="col"></th>
+                                <th scope="col">Id</th>
+                                <th scope="col">Descrição</th>
+                                <th scope="col">Valor</th>
+                                <th scope="col">Código de barras</th>
+                                <th scope="col">Imagem</th>
+                                <th scope="col"></th>
                             </tr>
                         </thead>
                         <tbody>
                             <?php for($i = 0; $i < 25; $i++)
                             {
-                                //
                                 echo "<tr>
-                                <td>Id $i</th>
-                                <td>Descrição $i</td>
-                                <td>Valor $i</td>
-                                <td>Código de barras $i</td>
-                                <td>Imagem $i </td>
-                                <td>
-                                    <a href=' ' class='btn btn-warning'>
-                                        Editar
-                                    </a>
-                                    <a href=' ' class='btn btn-danger'>
-                                        Excluir
-                                    </a>
-                                </td>
-                                    </tr>
-                                ";
-                            }?>
+                                        <td>Id $i</td>
+                                        <td>Descrição $i</td>
+                                        <td>Valor $i</td>
+                                        <td>Código barras $i</td>
+                                        <td>Imagem $i</td>
+                                        <td>
+                                            <a href='' class='btn btn-warning'>
+                                                Editar
+                                            </a>
+                                             <a href='' class='btn btn-danger'>
+                                                Excluir
+                                            </a>
+                                        </td>
+                                    </tr>";
+                            }
+                            ?>
+                                                    
                         </tbody>
                         </table>
                     </div>
                 </div>
+
             </div>
         </div>
     </div>
 </div>
+
 <?php include "rodape.php"; ?>
