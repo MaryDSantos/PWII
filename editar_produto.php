5a83f42ac4f5f64b6684ddf4a0d72e8284326370
marianaluan <mariana.dos.santoss@outlook.com>
2024-11-07 22:28:09 -0300

    Aula07

--- editar_produto.php
+++ editar_produto.php
@@ -10,7 +10,8 @@ if( isset($_POST['id']) && !empty($_POST['id']) &&
     include "conexao.php";
     $sql = "UPDATE PRODUTOS SET Descricao = '$_POST[descricao]',
                                 Valor = '$_POST[valor]',
-                                Codigo_Barras = '$_POST[codigo_barras]'
+                                Codigo_Barras = '$_POST[codigo_barras]',
+                                Categoria_Id = $_POST[Categoria_Id]
                                 WHERE Id = $_POST[id]";
     //echo $sql;
     $resultado = $conexao->query($sql);
@@ -26,7 +27,7 @@ if( isset($_POST['id']) && !empty($_POST['id']) &&
 if ( isset($_GET["Id"]) && !empty( $_GET['Id'] )   )  
 {
     include "conexao.php";
-    $sql = "Select Id, Descricao, Valor, Codigo_Barras from produtos where Id = $_GET[Id]";
+    $sql = "Select Id, Descricao, Valor, Codigo_Barras, Categoria_Id from produtos where Id = $_GET[Id]";
     $resultado = $conexao->query($sql);
     if($resultado)
     {
@@ -38,6 +39,7 @@ if ( isset($_GET["Id"]) && !empty( $_GET['Id'] )   )
                 $descricao = $row["Descricao"];
                 $valor = $row["Valor"];
                 $codigo_barras = $row["Codigo_Barras"];
+                $Categoria_Id = $row["Categoria_Id"];
             }
         }
         else
@@ -65,6 +67,31 @@ else
     <input name="descricao" value="<?php echo $descricao ?>" />
     <input name="valor" value="<?php echo $valor ?>" />
     <input name="codigo_barras" value="<?php echo $codigo_barras ?>" />
+    <select name="Categoria_Id">
+        <?php
+        $sql_categorias = "Select Id, Nome from Categorias";
+        $resultado_categoria =$conexao->query($sql_categorias);
+        if ($resultado_categoria->num_rows > 0) 
+        {
+            while($row = $resultado_categoria->fetch_assoc()) 
+            {
+                if($Categoria_Id == $row["Id"])
+                {
+                echo "<option selected value='$row[Id]'> $row[Nome] </option>";
+                }
+                else
+                {
+                echo "<option value='$row[Id]'> $row[Nome] </option>";
+                }
+            }
+        }else
+        {
+            echo "<option value='0'> Não tem categoria cadastrada </option>";
+        }
+        ?>
+
+    </select>
+    <br>
     <button type="submit" >
         Salvar Alterações
     </button>
